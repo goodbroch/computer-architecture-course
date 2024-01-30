9d8a48cea68b0b582e544f0b416fde1dff4f5d01
Александр Прядкин <goodbroch@gmail.com>
2024-01-30 17:21:47 +0300

    feat: add cards and titlepage

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,15 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Аппаратное обеспечение вычислительных систем</span>,
+  editLink: {
+    component: null
+  },
+  feedback: {
+    content: null
+  },
+  toc: {
+    title: (<>Навигация</>)
+  }
 }
 
 export default config
